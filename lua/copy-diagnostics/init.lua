1a6b15b9ca2abf433a1fa1117d0bd0f797112a35
Nicholas Stafford <ngstafford1@gmail.com>
2025-01-30 12:18:02 -0500

    moved logic to the lua folder

--- lua/copy-diagnostics/init.lua
+++ lua/copy-diagnostics/init.lua
@@ -6,9 +6,29 @@
 ---@module 'copy_diagnostics'
 ---
 
-local configuration = require("copy_diagnostics._core.configuration")
 local M = {}
 
+local configuration = require("copy-diagnostics._core.configuration")
 configuration.initialize_data_if_needed()
+
+vim.keymap.set("n", configuration.DATA.keymap, function()
+    local diagnostics = vim.diagnostic.get(0, {})
+    if not diagnostics or #diagnostics == 0 then
+        print("No next diagnostic")
+        return
+    end
+
+    -- Extract the diagnostic messages
+    local diagnostic_messages = {}
+    for _, diagnostic in ipairs(diagnostics) do
+        table.insert(diagnostic_messages, diagnostic.message)
+    end
+
+    -- Join the messages into a single string
+    local diagnostic_text = table.concat(diagnostic_messages, "\n")
+    -- Copy to system clipboard
+    vim.fn.setreg("+", diagnostic_text)
+    print("Copied diagnostics to clipboard.")
+end, { desc = "Copy Diagnostics" })
 
 return M
